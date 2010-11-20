17254d220b78f1a3e706af4090902a56fd65ce53
Aaron <aaron.l.madison@gmail.com>
2010-11-19 22:24:21 -0600

    changed model name. Adding views

--- src/contact_form/models.py
+++ src/contact_form/models.py
@@ -1,13 +1,14 @@
+
 from django.db import models
 
-class ContactData(models.Model):
+class ContactEmail(models.Model):
     """
     ContactData model to store contact information in database
     """
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=11, blank=True)
-    message = models.CharField(max_length=1000)
+    message = models.TextField()
     creationtime = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
--- src/contact_form/views.py
+++ src/contact_form/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView
+
+from contact_form.forms import ContactForm
+
+
+def completed(request):
+    from django import http
+    return http.HttpResponse("completed")
+
+class ContactPage(CreateView):
+    template_name = "contact_form/contact_form.html"
+    form_class = ContactForm
+
+    def get_success_url(self):
+        return reverse("contact_form:completed")
