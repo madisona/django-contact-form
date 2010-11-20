3fed90f65d7a9d8595c031c9909e3ff12f8cde34
Aaron <aaron.l.madison@gmail.com>
2010-11-19 20:03:58 -0600

    removed unused requirements. added docutils

--- setup.py
+++ setup.py
@@ -10,13 +10,10 @@ setup(
     package_dir={'': 'src'},
     install_requires = (
 #        'django',       # commenting out... using django 1.3 alpha 1 for now
+        'docutils',      # so we can use django admin documentation
         'mock',          # used for testing purposes
         'django-debug-toolbar',
 #        'pyyaml',        # useful for fixtures and testing
 #        'south',         # incredibly useful for database migrations
-#        'coverage',      # useful for continuous integration, tells how much of code is covered by tests 
-#        'clonedigger',   # useful for continuous integration, looks for duplicate chunks of code
-#        'unittest-xml-reporting', # useful for continuous integration, makes nice test results
-#        'pylint',        # useful for continuous integration, looks at code quality
     ),
 )
