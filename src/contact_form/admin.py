4b1556504b3b3f5e5eb545bfe9b44fa5d1de77ce
Aaron <aaron.l.madison@gmail.com>
2010-11-19 22:22:20 -0600

    customized admin display

--- src/contact_form/admin.py
+++ src/contact_form/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 from contact_form import models
 
+class ContactEmailAdmin(admin.ModelAdmin):
+    model = models.ContactEmail
+    search_fields = ("name", "email", "message")
+    list_display = ("name", "email", "creationtime")
 
-
-
-admin.site.register(models.ContactData)
+admin.site.register(models.ContactEmail, ContactEmailAdmin)
