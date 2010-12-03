020c772e4b7f152abecbaa1d727dddf0185fb376
Aaron Madison <aaron.madison@theimtgroup.com>
2010-12-03 16:23:23 -0600

    moved django out of src and into buildout requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
     name="django-project",
     version="0.1",
-    description="Django app using buildout",
+    description="Django contact form app using buildout",
     author="Aaron Madison",
-
+    packages=('contact_form',),
     package_dir={'': 'src'},
     install_requires = (
-#        'django',       # commenting out... using django 1.3 alpha 1 for now
+        'django==1.3-alpha-1',       # commenting out... using django 1.3 alpha 1 for now
         'unittest2',
         'docutils',      # so we can use django admin documentation
         'mock',          # used for testing purposes
@@ -17,4 +17,5 @@ setup(
 #        'pyyaml',        # useful for fixtures and testing
 #        'south',         # incredibly useful for database migrations
     ),
+    dependency_links=('http://www.djangoproject.com/download/1.3-alpha-1/tarball/#egg=django-1.3-alpha-1',),
 )
